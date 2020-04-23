19b4951c758e0133d20fa104e87bc7969da0f256
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-23 19:48:14 +0200

    test getItem

--- CustomLinkProvider/Test/DynamoDB.ts
+++ CustomLinkProvider/Test/DynamoDB.ts
@@ -1,4 +1,19 @@
-
-import AWS from 'aws-sdk';
+let AWS = require("aws-sdk");
 AWS.config.update({ region: 'us-east-1' });
 var dynamodb = new AWS.DynamoDB();
+
+
+var params = {
+    Key: {
+        "email": {
+            S: "test@gmail.com"
+        },
+    },
+    TableName: "Users_Info"
+};
+
+dynamodb.getItem(params, function (err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log(data);
+
+});
