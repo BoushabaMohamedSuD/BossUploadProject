429aa486afdce837167de8605fb03024391d9820
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-23 20:00:38 +0200

    put item test

--- CustomLinkProvider/Test/DynamoDB.ts
+++ CustomLinkProvider/Test/DynamoDB.ts
@@ -6,7 +6,7 @@ var dynamodb = new AWS.DynamoDB();
 var params = {
     Key: {
         "email": {
-            S: "test@gmail.com"
+            S: "test1@gmail.com"
         },
     },
     TableName: "Users_Info"
@@ -17,3 +17,25 @@ dynamodb.getItem(params, function (err, data) {
     else console.log(data);
 
 });
+
+/*
+var params = {
+    Item: {
+        "email": {
+            S: "test1@gmail.com"
+        },
+        "Artist": {
+            S: "No One You Know"
+        },
+        "SongTitle": {
+            S: "Call Me Today"
+        }
+    },
+    ReturnConsumedCapacity: "TOTAL",
+    TableName: "Users_Info"
+};
+dynamodb.putItem(params, function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data);           // successful response
+
+});*/
