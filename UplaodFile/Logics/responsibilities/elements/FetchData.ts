109aefd9805ce565f545116b7cc46348f2e333ce
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-24 18:50:34 +0200

    fetch data respo

--- UplaodFile/Logics/responsibilities/elements/FetchData.ts
+++ UplaodFile/Logics/responsibilities/elements/FetchData.ts
@@ -1,14 +1,31 @@
 import { ResponsibilitiesHolder } from './../holders/ResponsibilitiesHolder';
 export class FetchData implements ResponsibilitiesHolder {
 
-
+    private Nextchaine!: ResponsibilitiesHolder;
     public setNextChaine(chaine: ResponsibilitiesHolder): ResponsibilitiesHolder {
-        return chaine;
+        this.Nextchaine = chaine;
+        return this.Nextchaine;
     }
 
     public process(): Promise<any> {
         return new Promise((resolve, reject) => {
+            // code her
 
+            if (this.Nextchaine != null) {
+                console.log('going to next chaine');
+                this.Nextchaine.process()
+                    .then((data) => {
+                        resolve(data);
+
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        console.log('Error');
+                        reject(err);
+                    });
+            } else {
+                console.log('this is the end of the chaine');
+            }
         })
     };
 
