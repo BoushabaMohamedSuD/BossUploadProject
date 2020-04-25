3f201eb756f83009018aafcf655a9d5ecfc1082c
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-25 04:06:29 +0200

    Filter resp logic

--- UplaodFile/Logics/templates/CheckSize.ts
+++ UplaodFile/Logics/templates/CheckSize.ts
@@ -1,4 +1,15 @@
 export function CheckSize(data: any): boolean {
     console.log('Check Size');
-    return true;
+    let consumedSize = data.data.consSize;
+    let maxSize = data.data.maxSize;
+    let fileSize = data.data.request.fileSize;
+    if ((fileSize + consumedSize) < maxSize) {
+        console.log("still have space");
+        return true;
+    } else {
+        console.log("free size consumed");
+        return false;
+    }
+
+
 }
--- UplaodFile/Logics/templates/CheckStatus.ts
+++ UplaodFile/Logics/templates/CheckStatus.ts
@@ -1,4 +1,11 @@
 export function CheckStatus(data: any): boolean {
     console.log('Check Status');
-    return true;
+    if (data.data.status == "Normal") {
+        console.log("status confirmed");
+        return true;
+    } else {
+        console.log("status is not confirmed")
+        return false;
+    }
+
 }
