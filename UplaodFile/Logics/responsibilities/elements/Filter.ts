0c325349b1c58fae85e516afff1a11c0b483abec
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-25 20:08:58 +0200

    file size check templet uplaod file integration

--- UplaodFile/Logics/responsibilities/elements/Filter.ts
+++ UplaodFile/Logics/responsibilities/elements/Filter.ts
@@ -1,6 +1,7 @@
 import { ResponsibilitiesHolder } from './../holders/ResponsibilitiesHolder';
 import { CheckSize } from '../../templates/CheckSize';
 import { CheckStatus } from '../../templates/CheckStatus';
+import { CheckFileSize } from '../../templates/CheckFileSize';
 export class Filter implements ResponsibilitiesHolder {
 
     private Nextchaine!: ResponsibilitiesHolder;
@@ -18,9 +19,11 @@ export class Filter implements ResponsibilitiesHolder {
     public process(): Promise<any> {
         return new Promise((resolve, reject) => {
             // code her
+            let err: string[] = [];
             let respSize: boolean = CheckSize(this.data);
             let respStatus: boolean = CheckStatus(this.data);
-            if (respSize && respStatus) {
+            let respFileSize: boolean = CheckFileSize(this.data);
+            if (respSize && respStatus && respFileSize) {
                 //if evrything is ok
                 if (this.Nextchaine != null) {
                     console.log('going to next chaine');
@@ -47,11 +50,17 @@ export class Filter implements ResponsibilitiesHolder {
 
                 if (!respStatus) {
                     console.log("error checking status");
-                    reject("error checking status");
-                } else {
-                    console.log("error checking size ");
-                    reject("error checking size");
+                    err.push("error checking status");
                 }
+                if (!respSize) {
+                    console.log("error checking size");
+                    err.push("error checking size")
+                }
+                if (!respFileSize) {
+                    console.log("error checking file size");
+                    err.push("error checking file size");
+                }
+                reject(err);
             }
 
         })
