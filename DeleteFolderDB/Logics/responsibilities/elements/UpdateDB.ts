e141445ff8a99497804d95a618bd5762606abd66
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-27 04:29:12 +0200

    UpdateDB Logic

--- DeleteFolderDB/Logics/responsibilities/elements/UpdateDB.ts
+++ DeleteFolderDB/Logics/responsibilities/elements/UpdateDB.ts
@@ -2,6 +2,7 @@ import { ResponsibilitiesHolder } from './../holders/ResponsibilitiesHolder';
 let AWS = require("aws-sdk");
 AWS.config.update({ region: 'us-east-1' });
 const s3 = new AWS.S3();
+let dynamodb = new AWS.DynamoDB();
 
 
 export class UpdateDB implements ResponsibilitiesHolder {
@@ -22,34 +23,78 @@ export class UpdateDB implements ResponsibilitiesHolder {
         return new Promise((resolve, reject) => {
             // code here
 
+            let email = this.data.request.email;
+            let consSize = this.data.data.consSize;
+
+
             let FolderSize = 0;
+
+
             this.data.
                 data.keys.forEach(element => {
                     FolderSize = FolderSize + element.size;
                 });
 
-            //if evrything is ok
-            if (this.Nextchaine != null) {
-                console.log('going to next chaine');
-                this.Nextchaine.process()
-                    .then((resp) => {
-                        // resp is her false or true
-                        if (resp) {
-                            resolve(resp);
-                        } else {
-                            reject(resp);
-                        }
 
-                    })
-                    .catch((err) => {
-                        // console.log(err);
-                        //console.log('Error');
-                        reject(err);
-                    });
-            } else {
-                console.log('this is the end of the chaine');
-                resolve(true);
-            }
+            let sizeconsumed = consSize - FolderSize;
+
+
+
+            let sizeconsumedString: string = sizeconsumed.toString()
+
+            let params = {
+                ExpressionAttributeNames: {
+                    "#y": "size_consumed"
+                },
+                ExpressionAttributeValues: {
+
+                    ":y": {
+                        N: sizeconsumedString
+                    }
+                },
+                Key: {
+                    "email": {
+                        S: email
+                    },
+
+                },
+                ReturnValues: "ALL_NEW",
+                TableName: "Users_Info",
+                UpdateExpression: "SET #Y = :y"
+            };
+
+            dynamodb.updateItem(params, (err, data) => {
+                if (err) {
+                    console.log(err, err.stack);
+                    reject("we cannot update consemed size in FilterUpdateObject");
+                }
+                else {
+                    //if evrything is ok
+                    if (this.Nextchaine != null) {
+                        console.log('going to next chaine');
+                        this.Nextchaine.process()
+                            .then((resp) => {
+                                // resp is her false or true
+                                if (resp) {
+                                    resolve(resp);
+                                } else {
+                                    reject(resp);
+                                }
+
+                            })
+                            .catch((err) => {
+                                //console.log(err);
+                                //console.log('Error');
+                                reject(err);
+                            });
+                    } else {
+                        console.log('this is the end of the chaine');
+                        resolve(true);
+                    }
+                }
+
+            });
+
 
 
 
