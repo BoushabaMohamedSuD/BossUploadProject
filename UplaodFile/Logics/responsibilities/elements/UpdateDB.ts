3a9705fc37a77dfac9f8adfdc24191c873db864d
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-27 20:47:32 +0200

    UpdateDB uploadFile

---
+++ UplaodFile/Logics/responsibilities/elements/UpdateDB.ts
@@ -0,0 +1,55 @@
+import { ResponsibilitiesHolder } from './../holders/ResponsibilitiesHolder';
+
+const AWS = require("aws-sdk");
+var s3 = new AWS.S3();
+export class GeneratorUrl implements ResponsibilitiesHolder {
+
+    private Nextchaine!: ResponsibilitiesHolder;
+    private data: any;
+
+    constructor(data: any) {
+        this.data = data;
+    }
+
+    public setNextChaine(chaine: ResponsibilitiesHolder): ResponsibilitiesHolder {
+        this.Nextchaine = chaine;
+        return this.Nextchaine;
+    }
+
+    public process(): Promise<any> {
+        return new Promise((resolve, reject) => {
+
+
+            // code here
+
+
+            //if evrything is ok
+            if (this.Nextchaine != null) {
+                console.log('going to next chaine');
+                this.Nextchaine.process()
+                    .then((resp) => {
+                        // resp is her false or true
+                        if (resp) {
+                            resolve(resp);
+                        } else {
+                            reject(resp);
+                        }
+
+                    })
+                    .catch((err) => {
+                        // console.log(err);
+                        //console.log('Error');
+                        reject(err);
+                    });
+            } else {
+                console.log('this is the end of the chaine');
+                resolve(true);
+            }
+
+
+
+
+        });
+    };
+
+}
